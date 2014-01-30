147f6f486cf4763942638e8edbf3214e67a9aa5c
Juan Carlos <juancarloscruzdiaz@gmail.com>
2014-01-30 00:12:07 -0500

    primer commit

---
+++ js/app.js
@@ -0,0 +1,102 @@
+var miembros = {"jorgeg": {	"nombre": "Jorge G. Ramírez Díaz",
+							"bio": "<p>Socio fundador y consultor. Abogado por la Pontificia Universidad Católica del Perú, con más de 50 años de ejercicio activo de la profesión en asesoramiento legal y procesos judiciales y arbitrales. Fue miembro del Jurado de Honor de la Magistratura y Presidente del Jurado de Honor de la Superintendencia Nacional de los Registros Públicos (SUNARP).</p><p> Integró Tribunales como magistrado suplente en el Poder Judicial, y es miembro de Tribunales Arbitrales de derecho y de conciencia del Centro de Conciliación y Arbitraje Nacional e Internacional de la Cámara de Comercio de Lima, del Centro de Análisis y Resolución de Conflictos de la Pontificia Universidad Católica del Perú (PUCP), del Centro de Arbitraje de American Chamber of Commerce of Perú (Amcham Perú), y del Centro de Conciliación y Arbitraje de la Superintendencia Nacional del Aseguramiento en Salud (SUNASA). </p><p>Miembro de las Comisiones Consultivas de Títulos Valores del Colegio de Abogados de Lima y de la Universidad de Lima. Consultor legal y expositor en conferencias y forums en las áreas del Derecho Civil, Comercial, Procesal Civil y Arbitral. Ha sido asesor y representante procesal de la Compañía Peruana de Teléfonos por 15 años, y de instituciones bancarias extranjeras y nacionales por 20 años. </p><p>Ha sido también profesor principal del curso de Contratos de Comercio y Títulos Valores en la Facultad de Derecho de la Pontificia Universidad Católica del Perú, y profesor de los cursos de Sociedades y Títulos Valores en la Facultad de Derecho y Ciencias Políticas de la Universidad de Lima, así como del curso de Títulos Valores en el Programa de Postgrado para Capacitación de Jueces de la Universidad Nacional Mayor de San Marcos y en la Academia de la Magistratura. Ha integrado comisiones de reforma legal de las normas procesales y de títulos valores vigentes. </p>", 
+	      					"foto": "images/abogado.jpg",
+	      					"pubs": []},
+				"jorgel": {	"nombre": "Jorge Luis Ramírez Zegarra",
+							"bio": "<p>Socio. Abogado por la Universidad de Lima, con más de 23 años de experiencia en la profesión, con estudios de postgrado en la Maestría de Derecho Empresarial de la misma Universidad. </p><p>Consultor de Empresas con especialidades en Derecho Civil, Corporativo, Laboral, Procesal Civil y Arbitral. Miembro del Instituto Peruano de Derecho Mercantil. Arbitro del Centro de Conciliación y Arbitraje Nacional e Internacional de la Cámara de Comercio de Lima. Árbitro del Centro de Conciliación y Arbitraje de la Superintendencia Nacional del Aseguramiento en Salud (SUNASA). </p><p>Profesor del curso de Títulos Valores en la Facultad de Derecho y Ciencias Políticas de la Universidad de Lima.</p>",
+							"foto": "images/abogado.jpg",
+							"pubs": [
+							'"La escisión: sus aspectos societarios y tributarios", en coautoría con Luis Felipe Velarde Koechlin, publicado en Advocatus Nº 25, Revista editada por alumnos de la Facultad de Derecho de la Universidad de Lima, 2011-2.', 
+							'"El Contrato de Trabajo: concepto, modalidades y diferencia con la locación de servicios", en coautoría con Luis Felipe Velarde Koechlin, publicado en Advocatus Nº 23, Revista editada por alumnos de la Facultad de Derecho de la Universidad de Lima, 2010-2.',
+							'"La utilización de la Factura Conformada en nuestro medio frente al Pagaré y la Letra de Cambio", publicado en Advocatus N° 20, Revista editada por alumnos de la Facultad de Derecho de la Universidad de Lima, 2009-1.',
+							'"El Cheque en la Ley de Títulos Valores", publicado en el libro por los XXV Años de la Facultad de Derecho de la Universidad de Lima, Fondo Editorial, primera edición, Lima, 2006.',
+							'"El Pagaré en la nueva Ley de Títulos Valores", publicado en el Tratado de Derecho Mercantil, Tomo II, del Instituto Peruano de Derecho Mercantil en coedición con Gaceta Jurídica, primera edición, Lima, 2004.',
+							'"Evolución e importancia del Pagaré y su tratamiento en la nueva Ley de Títulos Valores (Ley 27287)", publicado en la revista jurídica Ius et Praxis N° 32 de la Facultad de Derecho de la Universidad de Lima, Fondo Editorial, Lima, 2001.']},
+				"luisf": {	"nombre": "Luis Felipe Velarde Koechlin",
+							"bio": "<p>Socio. Abogado por la Universidad de Lima. Máster en Derecho Empresarial por la Universidad de Lima, con doble grado por la Universidad Autónoma de Madrid (UAM). Consultor de empresas en las áreas de Derecho Tributario, Planeamiento Tributario, Revisiones Contable-Tributarias y Derecho Corporativo. Árbitro del Centro de Conciliación y Arbitraje de la Superintendencia Nacional del Aseguramiento en Salud (SUNASA).</p><p>Experiencia profesional anterior: BDO Perú (2004 - 2005); Ernst & Young (2006 - 2007); Berninzon, Loret de Mola, Benavides & Fernández, Abogados (2007-2009).</p>",
+							"foto": "images/abogado.jpg",
+							"pubs": [
+							'"La escisión: sus aspectos societarios y tributarios", en coautoría con Jorge Luis Ramírez Zegarra, publicado en Advocatus Nº 25, Revista editada por alumnos de la Facultad de Derecho de la Universidad de Lima, 2011-2.',
+							'"El Contrato de Trabajo: concepto, modalidades y diferencia con la locación de servicios", en coautoría con Jorge Luis Ramírez Zegarra, publicado en Advocatus Nº 23, Revista editada por alumnos de la Facultad de Derecho de la Universidad de Lima, 2010-2.',
+							'"Leasing y depreciación acelerada: Simple es mejor", publicado en Advocatus, revista editada por los alumnos de la Facultad de Derecho de la Universidad de Lima. Edición N° 21, Lima, 2009-2.',
+							'"La calificación de los Hechos Económicos en la Norma VIII del Título Preliminar del Código Tributario: ¿es una facultad o deber de la Administración Tributaria?", publicado en el libro "Tributación y Derecho: Liber in memoriam Juan Lino Castillo Vargas", compilado por los doctores Mario Castillo Freyre, Jorge Luis Collantes González e Ignacio López de Romaña, editorial Palestra, Biblioteca del Estudio Mario Castillo Freyre, Lima, 2009.']},
+				"ximena": {	"nombre": "Ximena Ramírez Zegarra",
+							"foto": "images/abogado.jpg",
+							"bio": "<p>Abogada por la Universidad de Lima, con más de 18 años de experiencia profesional a nivel nacional e internacional. </p><p>Máster en Derecho por la Ruprecht-Karls-Universitaet de Heidelberg, Alemania. </p><p>Máster en Psicología por la Escuela Europea de Negocios - Columbus I.B.S. de Aragón, España. </p><p>Diplomada en Formación Pedagógica por la Universidad de Piura (Profohrum), Perú. </p><p>Especialista en el manejo de resolución de conflictos familiares, con especialidades en Derecho Civil y Derecho de Familia. </p><p>Árbitro del Centro de Conciliación y Arbitraje Nacional e Internacional de la Cámara de Comercio de Lima. Conciliadora extrajudicial del Centro de Conciliación y Arbitraje de la Superintendencia Nacional del Aseguramiento en Salud (SUNASA).</p>",
+							"pubs": []},
+				"bernardo": {	"nombre": "Bernardo Velarde Rivero",
+							"foto": "images/abogado.jpg",
+							"bio": "<p>Abogado por la Pontificia Universidad Católica del Perú, con más de 48 años de ejercicio activo de la profesión en asesoramiento legal y patrocinio de juicios civiles y penales. Integró el Departamento Legal del Banco Popular del Perú desde 1973 hasta su liquidación en 1993. Ha sido miembro del Cuerpo Jurídico de la Marina de Guerra del Perú desde 1968 hasta 1996, desempeñando diversos cargos de asesoramiento administrativo y ejerciendo el cargo de Procurador Público de la Marina de Guerra del Perú desde 1991 hasta 1996, año en que pasó a la Situación de Retiro con el grado de Contralmirante CJ. A partir de 1996 se ha desempeñado como abogado externo de la Compañía Volvo del Perú S.A. en asuntos de recuperación de créditos y patrocinio de juicios penales.</p>",
+							"pubs": []},
+				"jkosut": {	"nombre": "Jorge Luis Kossuth Maccera",
+							"foto": "images/abogado.jpg",
+							"bio": "<p>Abogado por la Pontificia Universidad Católica del Perú, con más de 38 años de experiencia en la profesión en las áreas de Derecho Civil, Bancario, de Seguros y Procesal Civil; habiéndose desempeñado como asesor en empresas bancarias y de seguros.</p>",
+							"pubs": []},
+				"patricia": {	"nombre": "Patricia Conroy de Mastrokalos",
+							"foto": "images/abogado.jpg",
+							"bio": "<p>Abogada por la Universidad de Lima, con más de 20 años de experiencia en la profesión, con estudios de postgrado en Derecho Registral y Notarial en el Colegio de Abogados de Lima. </p><p>Integró la Comisión Consultiva de Derecho Tributario del Colegio de Abogados de Lima (2007 - 2011). </p><p>Fue miembro de la Comisión Consultiva de Derecho Registral del Colegio de Abogados de Lima (2007). </p><p>Asesora de personas jurídicas con las especialidades en Derecho Corporativo, Civil, Tributario, Inmobiliario, Registral y saneamiento legal de bienes inmuebles.</p>",
+							"pubs": []},
+				"thalia": {	"nombre": "Thalía Olenka Cárdenas García",
+							"foto": "images/abogado.jpg",
+							"bio": "<p>Egresada de la Facultad de Derecho y Ciencia Política de la Universidad Nacional Mayor de San Marcos (UNMSM), con experiencia en Derecho Corporativo y Derecho Procesal Civil. </p><p>Asistente de docencia del curso de Derecho Societario y Títulos Valores en la misma casa de estudios. </p><p>Ex-coordinadora general del Taller de Derecho Empresarial “Ulises Montoya Manfredi”. </p><p>Ha publicado diversos artículos de Derecho Empresarial en boletines de la Facultad de Derecho de la misma UNMSM.</p>",
+							"pubs": []}
+				};
+
+
+$.expander.defaults.slicePoint = 350;
+$(document).ready(function() {
+
+	$('a.expand-bio').on('click', function(e) {
+		e.preventDefault();
+		$(".bio-wrapper").slideUp();
+		var m = $(this).attr("data-miembro");
+		var data = miembros[m];
+		$(".nombre").html(data.nombre);
+		$(".bio").html(data.bio);
+		$(".foto").attr("src",data.foto);
+		if(data.pubs.length != 0){
+			$(".pubs-title").show();
+			$(".pubs").show();
+			$(".pubs").find("li").remove();
+			$.each(data.pubs, function(index, item) {
+			$(".pubs").append("<li>" + item + "</li>");
+		});
+		} else {
+			$(".pubs-title").hide();
+			$(".pubs").hide();
+		}
+		
+		$(".bio-wrapper").slideDown();
+	});
+
+	$('div.expandable').expander({
+  	  expandText: 'leer más',
+	  expandPrefix: '… ',
+	
+	  // class names for summary element and detail element
+	  summaryClass: 'summary',
+	  detailClass: 'details',
+	
+	  // one or more space-separated class names for span around
+	  // "read-more" link and "read-less" link
+	  moreClass: 'read-more',
+	  lessClass: 'read-less',
+	
+	  // number of milliseconds after text has been expanded at which to collapse the text again.
+	  // when 0, no auto-collapsing
+	  collapseTimer: 0,
+	
+	  // effects for expanding and collapsing
+	  expandEffect: 'fadeIn',
+	  expandSpeed: 250,
+	  collapseEffect: 'fadeOut',
+	  collapseSpeed: 200,
+	
+	  // allow the user to re-collapse the expanded text.
+	  userCollapse: true,
+	
+	  // text to use for the link to re-collapse the text
+	  userCollapseText: 'ocultar',
+	  userCollapsePrefix: ' ',
+  });
+});
---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+//<![CDATA[
+		
+		jQuery(window).load(function() { // makes sure the whole site is loaded
+			jQuery("#status").fadeOut(); // will first fade out the loading animation
+			jQuery("#preloader").delay(350).fadeOut(1000); // will fade out the white DIV that covers the website.
+		})
+		
+	//]]>
